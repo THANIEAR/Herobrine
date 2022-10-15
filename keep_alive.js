51d52c4c48255aac22b149d93bba368aa08fe632
THANIEAR <115701589+THANIEAR@users.noreply.github.com>
2022-10-15 11:44:42 +0530

    Delete keep_alive.js

--- keep_alive.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const app = express();
-const port = 2323;
-app.get('/', (req, res) => res.send('Afk bot!'));
-
-app.listen(port, () => console.log(`Afk bot is listening to http://localhost:${port}`));
